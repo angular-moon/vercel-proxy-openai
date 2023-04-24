cae36026ccf087eee7d29a5c75bcba2a0458f390
angular-moon <xiaoxuss@gmail.com>
2023-04-24 17:30:36 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,11 +2,7 @@ module.exports = {
   async rewrites() {
     return [
       {
-        source: "/chat/:match*",
-        destination: "https://chat.openai.com/:match*",
-      },
-      {
-        source: "/api/:match*",
+        source: "/:match*",
         destination: "https://api.openai.com/:match*",
       },
     ];
