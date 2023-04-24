ce2ef5bade82b0b0bc57ab050f51ee5b21afe084
angular-moon <xiaoxuss@gmail.com>
2023-04-24 17:38:40 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@ module.exports = {
   async rewrites() {
     return [
       {
+        source: "/webhp",
+        destination: "https://www.google.com/webhp",
+      },
+      {
         source: "/:path*",
         destination: "https://api.openai.com/:path*",
       },
